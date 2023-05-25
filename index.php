0998459f39767604ac0cebd90b271ba7b234e616
Roman <11-roman@mail.ru>
2023-05-25 09:50:27 +0300

    сделал поиск на главной странице, установил шрифты для шапки и блока поиска. Поиск методом post, обработка на странице check_post.php

--- index.php
+++ index.php
@@ -3,8 +3,15 @@
     require "blocks/header.php";
 ?>
 
+    <form action="check_post.php" method="post" class="search_block">
+        <input type="text" name="search" id="search" placeholder="Введите слово.." class="field_search"><br>
+        <!-- <label for="search">Быстрый поиск</label> -->
+        <input type="submit" value="Начать" class="search_button"><br>
+
+    </form>
+
 <section> <!-- в работе -->
-    <form action="#" id="aside">
+    <form id="aside">
         <fieldset>
             <legend>Информация обо мне:</legend>
         </fieldset>
@@ -12,11 +19,7 @@
 </section>
 
 <nav> <!-- в работе -->
-    <p>Rano ili pozdno vse polu4itsia</p>
-</nav>
-
-<nav> <!-- в работе -->
-    <p>Rano ili pozdno vse polu4itsia</p>
+    <p>Рано или поздно все получится!</p>
 </nav>
 
 <?php
