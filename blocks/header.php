a041864eafa55f5087cd93b131544fa9e7644ec9
Roman <11-roman@mail.ru>
2023-06-04 19:40:33 +0300

    перенес поиск в хедер, сделав его меньше, чем он был, в одну линию, в кнопку поиска добавил картинку

--- blocks/header.php
+++ blocks/header.php
@@ -10,8 +10,16 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
+
 <body>
 
-<header>
-
+<header class="header">
+    <div>
+        <nav>
+            <form action="check_post.php" method="post" class="search_block">
+                <input type="text" name="search" id="search" placeholder="Поиск.." class="field_search">
+                <input type="image" src="img/search.png" class="search_button">
+            </form>
+        </nav>
+    </div>
 </header>
