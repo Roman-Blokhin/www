cdbfd117b75a8921d8d4d6a0cadd40b3338d3d16
Roman <11-roman@mail.ru>
2023-06-07 16:53:14 +0300

    создал вторую линию с помощью контейнера div, поместил туда 3 блока, присвоив стили, как и у блока на 1 линии

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     require "blocks/header.php";
 ?>
 
-<main>
+<main class="index_div_1">
 
     <?php
         require "blocks/menu.php"
@@ -12,6 +12,15 @@
     <section class="index_div_line_1"></section>
 
 </main>
+
+<div class="index_div_2">
+    <section class="index_div_line_2_1"></section>
+
+    <section class="index_div_line_2_2"></section>
+    <section class="index_div_line_2_3"></section>
+
+</div>
+
 
 <?php
     require "blocks/footer.php";
