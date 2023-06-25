cf991d11e80095549fbaa0d4095e8d46bd23b828
Roman <11-roman@mail.ru>
2023-06-25 20:54:00 +0300

    опустил футер ниже, но поехал один блок на главной странице

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,6 +1,9 @@
 <footer>
-    <p>Все права защищены &copy; 2023</p>
+    <?php
+        echo "<br> <br> <br> <br> <br> <br>";
+        echo "<p>Все права защищены &copy; 2023</p>";
+    ?>
+
 </footer>
 </body>
 </html>
-
