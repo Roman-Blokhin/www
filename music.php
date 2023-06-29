6edfc8199380ebe2cde963b532c1167d5e1890e5
Roman <11-roman@mail.ru>
2023-06-29 21:44:51 +0300

    пытаюсь сделать несколько колонок в ряд на странице

--- music.php
+++ music.php
@@ -3,18 +3,32 @@
     require "blocks/header.php";
 ?>
 
-<div id="column" class="block_1">
+<!-- <div id="column" class="block_1">
     <div class="blocks_music_column_1">
         <a href="index.php">Тык</a>
     </div>
     <div class="blocks_music_column_1"></div>
-</div>
+</div> -->
 
 <div id="column" class="block_2">
-    <div class="blocks_music_column_2"></div>
-    <div class="blocks_music_column_2"></div>
-    <div class="blocks_music_column_2"></div>
-    <div class="blocks_music_column_2"></div>
+    <div class="row">
+        <div class="column">
+            <div class="red_column"></div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="green_column"></div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="blue_column"></div>
+        </div>
+    </div>
+
 </div>
 
 <?php
