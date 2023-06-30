2550f62a2ed36cbb6f1d38d53628f269e8194188
Roman <11-roman@mail.ru>
2023-06-30 21:02:24 +0300

    во вкладке music.php вернул меню обратно

--- music.php
+++ music.php
@@ -3,31 +3,13 @@
     require "blocks/header.php";
 ?>
 
-<!-- <div id="column" class="block_1">
-    <div class="blocks_music_column_1">
-        <a href="index.php">Тык</a>
-    </div>
+<div id="column" class="block_1">
+
+    <?php
+        require "blocks/menu.php"
+    ?>
+
     <div class="blocks_music_column_1"></div>
-</div> -->
-
-<div id="column" class="block_2">
-    <div class="row">
-        <div class="column">
-            <div class="red_column"></div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="column">
-            <div class="green_column"></div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="column">
-            <div class="blue_column"></div>
-        </div>
-    </div>
 
 </div>
 
